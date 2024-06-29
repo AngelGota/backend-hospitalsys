e3ff06682e2bfbaa5a61102782b88356be188032
Angelo <gangelos630@gmail.com>
2024-06-29 06:07:48 -0500

    spring initializr

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# backend-hospitalsys
+# Webservice hospital in aws
 Backend developed under the hexagonal architecture, following good practices and clean clode, designed to be deployed on AWS and maintained under the serverless architecture.
---
+++ src/main/java/pro/angelogomez/hospital/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package pro.angelogomez.hospital.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
