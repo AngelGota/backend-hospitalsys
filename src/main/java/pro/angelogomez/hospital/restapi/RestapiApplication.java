66cdd42aa5ba13800e20ee4a540baf34785a3756
Angelo <gangelos630@gmail.com>
2024-07-01 13:25:41 -0500

    OpenAPIDefinition

--- src/main/java/pro/angelogomez/hospital/restapi/RestapiApplication.java
+++ src/main/java/pro/angelogomez/hospital/restapi/RestapiApplication.java
@@ -1,9 +1,12 @@
 package pro.angelogomez.hospital.restapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Hospital API", version = "1.0", description = "API para el sistema de hospital"))
 public class RestapiApplication {
 
 	public static void main(String[] args) {
