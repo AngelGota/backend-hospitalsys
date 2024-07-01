669bf57652590362dbf0bc35b7257ee41528671b
Angelo <gangelos630@gmail.com>
2024-07-01 05:35:53 -0500

    Nuevo Mapper: Especialidad

---
+++ src/main/java/pro/angelogomez/hospital/restapi/infrastructure/mapper/EspecialidadMapper.java
@@ -0,0 +1,23 @@
+package pro.angelogomez.hospital.restapi.infrastructure.mapper;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import pro.angelogomez.hospital.restapi.domain.model.Especialidad;
+import pro.angelogomez.hospital.restapi.infrastructure.entity.EspecialidadEntity;
+
+@Mapper(componentModel = "spring")
+public interface EspecialidadMapper {
+    @Mappings(
+            {
+                    @Mapping(source = "id", target = "id"),
+                    @Mapping(source = "nombre", target = "nombre"),
+                    @Mapping(source = "descripcion", target = "descripcion")
+            }
+    )
+    Especialidad toEspecialidad(EspecialidadEntity especialidadEntity);
+    Iterable<Especialidad> toEspecialidades(Iterable<EspecialidadEntity> especialidadEntities);
+    @InheritInverseConfiguration
+    EspecialidadEntity toEspecialidadEntity(Especialidad especialidad);
+}
