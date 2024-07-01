23656567143c112a8818a07e72177a2a2faad34a
Angelo <gangelos630@gmail.com>
2024-07-01 05:35:31 -0500

    Bining PacienteService con parametro Repository

--- src/main/java/pro/angelogomez/hospital/restapi/infrastructure/config/Beanconfiguration.java
+++ src/main/java/pro/angelogomez/hospital/restapi/infrastructure/config/Beanconfiguration.java
@@ -1,7 +1,15 @@
 package pro.angelogomez.hospital.restapi.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pro.angelogomez.hospital.restapi.application.PacienteService;
+import pro.angelogomez.hospital.restapi.domain.port.IPacienteRepository;
 
 @Configuration
 public class Beanconfiguration {
+
+    @Bean
+    public PacienteService pacienteService(IPacienteRepository iPacienteRepository) {
+        return new PacienteService(iPacienteRepository);
+    }
 }
