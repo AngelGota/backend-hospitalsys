6ed9e8491562634eb865b7069552898bab0db01b
Angelo <gangelos630@gmail.com>
2024-07-01 13:36:34 -0500

    Payload para manejo de response

--- src/main/java/pro/angelogomez/hospital/restapi/infrastructure/handler/PayloadPaciente.java
+++ src/main/java/pro/angelogomez/hospital/restapi/infrastructure/handler/PayloadPaciente.java
@@ -2,12 +2,11 @@ package pro.angelogomez.hospital.restapi.infrastructure.handler;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import pro.angelogomez.hospital.restapi.domain.model.Paciente;
+
 
 @Data
 @AllArgsConstructor
 public class PayloadPaciente {
     private String message;
-    private int code;
-    private Paciente paciente;
+    private Object object;
 }
