5fc1c48ba82461dafc569f96599f87c289196eb1
Angelo <gangelos630@gmail.com>
2024-07-01 13:25:21 -0500

    Bining de todos los service de capa application

--- src/main/java/pro/angelogomez/hospital/restapi/infrastructure/config/Beanconfiguration.java
+++ src/main/java/pro/angelogomez/hospital/restapi/infrastructure/config/Beanconfiguration.java
@@ -2,8 +2,8 @@ package pro.angelogomez.hospital.restapi.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pro.angelogomez.hospital.restapi.application.PacienteService;
-import pro.angelogomez.hospital.restapi.domain.port.IPacienteRepository;
+import pro.angelogomez.hospital.restapi.application.*;
+import pro.angelogomez.hospital.restapi.domain.port.*;
 
 @Configuration
 public class Beanconfiguration {
@@ -11,5 +11,30 @@ public class Beanconfiguration {
     @Bean
     public PacienteService pacienteService(IPacienteRepository iPacienteRepository) {
         return new PacienteService(iPacienteRepository);
+    }
+
+    @Bean
+    public RecetaService recetaService(IRecetaRepository iRecetaRepository) {
+        return new RecetaService(iRecetaRepository);
+    }
+
+    @Bean
+    public MedicoService medicoService(IMedicoRepository iMedicoRepository) {
+        return new MedicoService(iMedicoRepository);
+    }
+
+    @Bean
+    public MedicinaService medicinaService(IMedicinaRepository iMedicinaRepository) {
+        return new MedicinaService(iMedicinaRepository);
+    }
+
+    @Bean
+    public EspecialidadService especialidadService(IEspecialidadRepository iEspecialidadRepository) {
+        return new EspecialidadService(iEspecialidadRepository);
+    }
+
+    @Bean
+    public CitaService citaService(ICitaRepository iCitaRepository) {
+        return new CitaService(iCitaRepository);
     }
 }
